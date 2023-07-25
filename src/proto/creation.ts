6e9957977a65fdd08fb089b2990d145d31c9b6da
josue <josue.medina@pragma.com.co>
2023-07-25 11:11:51 -0500

    Fix lint

--- src/proto/creation.ts
+++ src/proto/creation.ts
@@ -1,7 +1,7 @@
-import type * as grpc from '@grpc/grpc-js';
-import type { MessageTypeDefinition } from '@grpc/proto-loader';
+import type * as grpc from '@grpc/grpc-js'
+import type { MessageTypeDefinition } from '@grpc/proto-loader'
 
-import type { CreationClient as _creationPackage_CreationClient, CreationDefinition as _creationPackage_CreationDefinition } from './creationPackage/Creation';
+import type { CreationClient as _creationPackage_CreationClient, CreationDefinition as _creationPackage_CreationDefinition } from './creationPackage/Creation'
 
 type SubtypeConstructor<Constructor extends new (...args: any) => any, Subtype> = {
   new(...args: ConstructorParameters<Constructor>): Subtype;
--- src/proto/creationPackage/Creation.ts
+++ src/proto/creationPackage/Creation.ts
@@ -2,8 +2,8 @@
 
 import type * as grpc from '@grpc/grpc-js'
 import type { MethodDefinition } from '@grpc/proto-loader'
-import type { CreationRequest as _creationPackage_CreationRequest, CreationRequest__Output as _creationPackage_CreationRequest__Output } from './CreationRequest';
-import type { CreationResponse as _creationPackage_CreationResponse, CreationResponse__Output as _creationPackage_CreationResponse__Output } from './CreationResponse';
+import type { CreationRequest as _creationPackage_CreationRequest, CreationRequest__Output as _creationPackage_CreationRequest__Output } from './CreationRequest'
+import type { CreationResponse as _creationPackage_CreationResponse, CreationResponse__Output as _creationPackage_CreationResponse__Output } from './CreationResponse'
 
 export interface CreationClient extends grpc.Client {
   CreateItem(metadata: grpc.Metadata, options?: grpc.CallOptions): grpc.ClientDuplexStream<_creationPackage_CreationRequest, _creationPackage_CreationResponse__Output>;
